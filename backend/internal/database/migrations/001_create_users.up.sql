7b23057a4a3f126708a5b80f4818939d2601a66f
cgaldan <chrisgaldan@gmail.com>
2026-04-15 21:43:02 +0300

    feat: upgrade user to include followers and following count

--- backend/internal/database/migrations/001_create_users.up.sql
+++ backend/internal/database/migrations/001_create_users.up.sql
@@ -9,6 +9,8 @@ CREATE TABLE IF NOT EXISTS users (
     gender TEXT,
     avatar_path TEXT,
     about_me TEXT,
+    following_count INTEGER DEFAULT 0,
+    followers_count INTEGER DEFAULT 0,
     is_public BOOLEAN DEFAULT TRUE,
     is_online BOOLEAN DEFAULT TRUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
