6bdad14214a18f2d12ee58b105dbbe8a2f1e89b0
cgaldan <chrisgaldan@gmail.com>
2026-04-18 19:10:13 +0300

    feat(content): Content service interface add to services
    - Create Post and post validation contained for now
    - Also unit testing for post creating

--- backend/internal/service/services.go
+++ backend/internal/service/services.go
@@ -7,12 +7,14 @@ import (
 )
 
 type Services struct {
-	Auth AuthServiceInterface
+	Auth    AuthServiceInterface
+	Content ContentServiceInterface
 }
 
 func NewServices(repos *repository.Repositories, logger *logger.Logger) *Services {
 	return &Services{
-		Auth: NewAuthService(repos.User, repos.Session, logger),
+		Auth:    NewAuthService(repos.User, repos.Session, logger),
+		Content: NewContentService(repos.Post, logger),
 	}
 }
 
@@ -21,4 +23,8 @@ type AuthServiceInterface interface {
 	Login(loginData domain.LoginRequest) (*domain.User, string, error)
 	Logout(sessionID string) error
 	ValidateSession(sessionID string) (*domain.User, error)
+}
+
+type ContentServiceInterface interface {
+	CreatePost(userID int, postData domain.CreatePostRequest) (*domain.Post, error)
 }
