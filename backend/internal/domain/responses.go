742aa3a6a3e054a8787ec8b893a149892682c32a
cgaldan <chrisgaldan@gmail.com>
2026-04-20 13:09:15 +0300

    feat(auth): auth handler to handle auth requests

---
+++ backend/internal/domain/responses.go
@@ -0,0 +1,8 @@
+package domain
+
+type AuthResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message,omitempty"`
+	User    *User  `json:"user,omitempty"`
+	Token   string `json:"token,omitempty"`
+}
